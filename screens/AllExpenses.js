02b8e5b22fb46edd50a6642a2035512a35dc4661
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-02-05 16:45:00 +0100

    added input

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,10 +1,16 @@
-import { useContext } from "react";
-import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
-import {ExpensesContext} from '../store/expenses-context';
+import { useContext } from 'react';
+
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
 
 function AllExpenses() {
-    const expensesCtx = useContext(ExpensesContext);
-    return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" fallbackText="No registered expenses found"/>
-}
+  const expensesCtx = useContext(ExpensesContext);
 
-export default AllExpenses;
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!"
+    />
+  )
+}
